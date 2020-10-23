fce7c594e13778f7806248fa18de8fa5a9cdb3a3
Nicolas Gonzalez Paz <ngonzalez@flowingcode.com>
2020-10-23 15:08:24 -0300

    test(demo): integrate commons demo

--- src/test/java/com/flowingcode/vaadin/addons/granitealert/GranitealertDemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/granitealert/GranitealertDemoView.java
@@ -20,78 +20,25 @@
 package com.flowingcode.vaadin.addons.granitealert;
 
 import com.flowingcode.vaadin.addons.DemoLayout;
-import com.flowingcode.vaadin.addons.granitealert.GraniteAlert.GraniteAlertLevel;
-import com.vaadin.flow.component.button.Button;
+import com.flowingcode.vaadin.addons.demo.impl.TabbedDemoImpl;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @SuppressWarnings("serial")
 @Route(value = "granite-alert", layout = DemoLayout.class)
 @StyleSheet("context://frontend/styles/granitealert/demo-styles.css")
 @CssImport(value = "./styles/granitealert/custom-granite-styles.css", themeFor = "granite-alert-mixin")
-public class GranitealertDemoView extends Div {
+public class GranitealertDemoView extends VerticalLayout {
 
-	Div container = new Div();
+	private static final String GRANITE_DEMO = "Granite Alert Demo";
+	private static final String GRANITE_SOURCE = "https://github.com/FlowingCode/GraniteAlertAddon/tree/master/src/test/java/com/flowingcode/vaadin/addons/granitealert/GranitealertDemo.java";
 
-	{
-		createAlert(null, "boring", "Welcome to the GraniteAlertAddon demo. Please click any button below.");
-
-		HorizontalLayout buttons = new HorizontalLayout();
-		buttons.setSpacing(true);
-
-		buttons.add(new Button(VaadinIcon.EXCLAMATION.create(), ev -> createErrorAlert()));
-		buttons.add(new Button(VaadinIcon.WARNING.create(), ev -> createWarningAlert()));
-		buttons.add(new Button(VaadinIcon.INFO.create(), ev -> createInfoAlert()));
-		buttons.add(new Button(VaadinIcon.CHECK.create(), ev -> createSuccessAlert()));
-		buttons.add(new Button(VaadinIcon.MEH_O.create(), ev -> createCustomAlert("boring")));
-		buttons.add(new Button(VaadinIcon.SMILEY_O.create(), ev -> createCustomAlert("funny")));
-
+	public GranitealertDemoView() {
+		TabbedDemoImpl<GranitealertDemo> grDemo = new TabbedDemoImpl<>(new GranitealertDemo(), GRANITE_DEMO,
+				GRANITE_SOURCE);
+		add(grDemo);
 		setSizeFull();
-
-		add(container, buttons);
 	}
-
-	private void createSuccessAlert() {
-		createAlert(GraniteAlertLevel.SUCCESS, null, "Success");
-	}
-
-	private void createCustomAlert(String className) {
-		createAlert(null, className, "Info");
-	}
-
-	private void createInfoAlert() {
-		createAlert(GraniteAlertLevel.INFO, null, "Info");
-	}
-
-	private void createWarningAlert() {
-		createAlert(GraniteAlertLevel.WARNING, null, "Warning");
-	}
-
-	private void createErrorAlert() {
-		createAlert(GraniteAlertLevel.ERROR, null, "Error");
-	}
-
-	private void createAlert(GraniteAlertLevel level, String className, String message) {
-		GraniteAlert alert = new GraniteAlert();
-		alert.setCompact(true);
-		if (level != null) {
-			alert.setLevel(level);
-		}
-		if (className != null) {
-			alert.addClassName(className);
-		}
-
-		alert.add(new Span(message));
-
-		Button button = new Button(VaadinIcon.CLOSE_CIRCLE.create());
-		button.addClickListener(ev -> container.remove(alert));
-		alert.add(button);
-		container.add(alert);
-	}
-
 }
